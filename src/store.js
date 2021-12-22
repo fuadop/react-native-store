92925ae3fdeba53c5509cd9297b0b98020d03d52
fuadop <fuadolatunji@gmail.com>
2021-12-22 12:22:14 +0100

    update babel and jest

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var AsyncStorage = require('@react-native-async-storage/async-storage');
+import AsyncStorage from '@react-native-async-storage/async-storage';
 var Model = require('./model.js');
 var Util = require('./util.js');
 
